a6af54c02858446cd024b7715900198569c2d12f
NesiAwesomeneess <77176216+NesiAwesomeneess@users.noreply.github.com>
2021-04-13 19:39:12 +0100

    Add files via upload

--- ColorPickerPrototype/CPT.gd
+++ ColorPickerPrototype/CPT.gd
@@ -1,50 +1,70 @@
 extends Node2D
+class_name RandomPaletteGenerator
 
+var divisor = 2
 export (int, 0, 60) var max_offset = 20
-export (int, 2, 8) var no_of_colors = 2
+export (int, 2, 8) var size = 2
+export (float) var min_dif
 onready var color_rect = ColorRect
 var color_buffer
 var color_s = 0
 var color_h = 0
+
+var swatch = []
+
+export (bool) var generate_panels = false
+
 var rng = RandomNumberGenerator.new()
 
 func _ready():
-	shuffle()
+	swatch = shuffle()
 
 func shuffle():
+	var colors = []
 	var color_v
 	rng.randomize()
 	color_s = rng.randi_range(0, 100)
 	color_h = rng.randi_range(0, 359)
-	if no_of_colors >= 1:
-		for x in range(no_of_colors):
-			color_v = rng.randi_range(50, 100)
-			var chance = rng.randi_range(0, x)
-			var color = Color(1, 1, 1)
-			if x % 2 == 0:
-				color_h = wrapf(color_h+180, 0, 360)
-			color_v += rng.randi_range(-max_offset / 4, max_offset / 2)
-			color_s += rng.randi_range(-max_offset / 2, max_offset / 2)
-			color_h += rng.randi_range(-max_offset / 2, max_offset / 2)
-			if chance == 3:
-				var offset = 90 / rng.randi_range(1, 4)
-				color_h = wrapf(color_h+offset, 0, 360)
-			color_h = clamp(color_h, 0, 360)
-			color_s = clamp(color_s, 0, 100)
-			color.v = range_lerp(color_v, 0, 100, 0, 1)
-			color.s = range_lerp(color_s, 0 ,100, 0, 1)
-			color.h = range_lerp(color_h, 0 ,360, 0, 1)
-			var blot = color_rect.new()
-			blot.color = color
-			color_buffer = color
-			print(color.to_html(false))
-			blot.rect_min_size = Vector2(40, 40)
-			$PaletteContainer.add_child(blot)
-		
-#		$Sprite.material.set("shader_param/color2", $PaletteContainer.get_child(1).color)
-#		$Sprite.material.set("shader_param/color4", $PaletteContainer.get_child(0).color)
+	for x in range(size):
+		color_v = rng.randi_range(50, 100)
+		var chance = rng.randi_range(0, x)
+		var color = Color(1, 1, 1)
+		if x % divisor == 0:
+			color_h = wrapf(color_h+180, 0, 360)
+		color_v += rng.randi_range(-max_offset / 2, max_offset / 2)
+		color_s += rng.randi_range(-max_offset / 2, max_offset / 2)
+		color_h += rng.randi_range(-max_offset / 2, max_offset / 2)
+		if chance == 3:
+			var offset = 90 / rng.randi_range(1, 4)
+			color_h = wrapf(color_h+offset, 0, 360)
+		if colors.size() > divisor:
+			if abs((colors[x-divisor].s *100)  - color_s) < min_dif:
+				color_s += max_offset
+			if abs((colors[x-divisor].v * 100) - color_v) < min_dif:
+				color_v += max_offset
+		color_h = (wrapf(color_h, 0, 360) / 360.0)
+		color_s = (wrapf(color_s, 0, 100) / 100.0)
+		color_v = (wrapf(color_v, 0, 100) / 100.0)
+		color.s = color_s
+		color.h = color_h
+		color.v = color_v
+		colors.append(color)
+	
+	if generate_panels:
+		for _color in colors:
+			var panel = color_rect.new()
+			panel.color = _color
+			panel.rect_min_size = Vector2(40, 40)
+			$PaletteContainer.add_child(panel)
+	
+	return colors
 
 func _on_Button_pressed():
 	for c in $PaletteContainer.get_children():
 		c.queue_free()
-	shuffle()
+	swatch = shuffle()
+
+func _on_Save_pressed():
+	var swatch_save = ResourceLoader.load("res://Prototypes/ColorPickerPrototype/SwatchSaves.tres")
+	swatch_save.saved_swatches.append(swatch)
+	var _mes = ResourceSaver.save("res://Prototypes/ColorPickerPrototype/SwatchSaves.tres", swatch_save)
---
+++ ColorPickerPrototype/Swatch.gd
@@ -0,0 +1,4 @@
+extends Resource
+class_name swatch
+
+export (Array, Array, Color) var saved_swatches
